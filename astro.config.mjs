cef4a61ee21f75c1e7926ca0578182f575d537c7
Ivan Siljic <isiljic03@proton.me>
2025-12-16 20:47:05 +0100

    Setup template for further documenting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,38 @@
+// astro.config.mjs
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Sunny Docs',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/IvanSiljic/docs',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Cellular Networks',
+          items: [
+            { label: 'Overview', slug: 'networks/cellular' },
+            { 
+              label: '1G',
+              items: [
+                { label: 'Overview', slug: 'networks/cellular/1g/overview' },
+                { label: 'History', slug: 'networks/cellular/1g/history'}
+              ]
+            },
+            // { label: '2G', slug: 'networks/cellular/2g' },
+            // { label: '3G', slug: 'networks/cellular/3g' },
+            // { label: '4G', slug: 'networks/cellular/4g' },
+            // { label: '5G', slug: 'networks/cellular/5g' },
+          ],
+        },
+      ]
+    }),
+  ],
 });
---
+++ src/content/docs/networks/cellular/1g/overview.md
@@ -0,0 +1,6 @@
+---
+title: 1G Cellular Networks Overview
+description: Foreword and high-level view of 1G–5G cellular systems.
+---
+
+## TODO
